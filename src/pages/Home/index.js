e593b06e18572ace54e574d1a060eea16acf355e
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2022-10-05 22:47:08 -0300

    home create

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,27 @@
-function Home(){
-    return(
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+////https://api.themoviedb.org/3/movie/550?api_key=7f0b8ce42d97e76a4bad04601a4497fb&language=pt-BR
+function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing",{
+                params:{
+                    api_key: "7f0b8ce42d97e76a4bad04601a4497fb",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+        }
+
+        loadFilmes();
+
+    }, [])
+
+    return (
         <div>
             <h1>Bem vindo a Home</h1>
         </div>
