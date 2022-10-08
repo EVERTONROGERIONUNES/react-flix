5fd28b9f43b62afc88a20fa9c21cb9e33778c32c
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2022-10-07 21:32:21 -0300

    load movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 ////https://api.themoviedb.org/3/movie/550?api_key=7f0b8ce42d97e76a4bad04601a4497fb&language=pt-BR
 function Home() {
 
     const [filmes, setFilmes] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing",{
-                params:{
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
                     api_key: "7f0b8ce42d97e76a4bad04601a4497fb",
                     language: "pt-BR",
                     page: 1,
                 }
             })
+
+            setFilmes(response.data.results.slice(0, 10))
+
         }
 
         loadFilmes();
@@ -22,8 +27,18 @@ function Home() {
     }, [])
 
     return (
-        <div>
-            <h1>Bem vindo a Home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}  alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
