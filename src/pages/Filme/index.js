d112978df1adcf76671597e180a3e3c4e255a63b
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2022-10-12 22:43:55 -0300

    page details

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,64 @@
-function Filme(){
-    return(
-        <div>
-            <h1>Filmes</h1>
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+import './filme-info.css';
+
+function Filme() {
+
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "7f0b8ce42d97e76a4bad04601a4497fb",
+                    language: "pt-BR",
+                }
+            })
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+
+                })
+        }
+
+        loadFilmes();
+
+        return () => {
+
+        }
+
+    }, [])
+
+    if (loading) {
+        return (
+            <div className='filme-info'>
+                <h2>Carregando Detalhes ...</h2>
+            </div>
+        )
+
+    }
+
+    return (
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button> <a href="#">Trailer</a></button>
+
+            </div>
+
         </div>
     )
 }
